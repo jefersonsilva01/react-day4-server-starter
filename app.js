c2395e6afedf301897f3cab0676b27b7383d9011
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-07-25 17:24:56 -0300

    add logic auth

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const cors = require('cors');
 // INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session
 // AND UN-COMMENT OUT FOLLOWING LINES:
 
-// const session       = require('express-session');
-// const passport      = require('passport');
+const session = require('express-session');
+const passport = require('passport');
 
-// require('./configs/passport');
+require('./configs/passport');
 
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
@@ -62,9 +62,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // ADD SESSION SETTINGS HERE:
 
+app.use(session({
+  secret: "some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
 
 // USE passport.initialize() and passport.session() HERE:
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
@@ -86,7 +92,6 @@ app.use('/', index);
 
 app.use('/api', require('./routes/project-routes'));
 app.use('/api', require('./routes/task-routes'));
-
-
+app.use('/api', require('./routes/auth-routes'));
 
 module.exports = app;
--- routes/project-routes.js
+++ routes/project-routes.js
@@ -12,7 +12,8 @@ router.post('/projects', (req, res, next) => {
   Project.create({
     title,
     description,
-    tasks: []
+    tasks: [],
+    owner: req.user._id
   })
     .then(response => {
       res.json(response);
